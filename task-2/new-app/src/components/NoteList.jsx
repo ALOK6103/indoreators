339f593cfc17e50779e912457390d7ed7cfed780
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-12-14 11:51:53 +0530

    code added

--- task-2/new-app/src/components/NoteList.jsx
+++ task-2/new-app/src/components/NoteList.jsx
@@ -6,13 +6,17 @@ const NoteList = ({ notes, deleteNote }) => {
     console.log(deleteNote)
 
   return (
-    <div>
+    <div style={{display:"grid", margin:"auto" ,gridTemplateColumns:"repeat(3,1fr)", paddingLeft:"10%", paddingRight:"10%"}}>
       {notes && notes.map((note, index) => (
-        <h4 key={index}>
+        <div style={{border:"1px solid black" , width:"200px", marginTop:"50px", padding:"20px"}}>
+        <h4 key={index} >
           {note.text}
-          <button onClick={() => deleteNote(note._id)}>Delete</button>
+          
         </h4>
+        <button onClick={() => deleteNote(note._id)}>Delete</button>
+        </div>
       ))}
+      
     </div>
   );
 };
